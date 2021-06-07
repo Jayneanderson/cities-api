7f3e89857b1425afd272624b6aa5809f67e2e397
Jayneanderson Santos <aprenderprogramacao001@gmail.com>
2021-06-06 23:48:09 -0300

    Iniciada a criação dos modelos Estado e Cidade com devidos atributos e validações

--- src/main/java/com/stylethree/citiesapi/controller/CountryResource.java
+++ src/main/java/com/stylethree/citiesapi/controller/CountryResource.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +25,14 @@ public class CountryResource {
 
 	@GetMapping
 	public ResponseEntity<Page<Country>> getCountries(Pageable page) {
-		return ResponseEntity.ok(repository.findAll(page));
+		return ResponseEntity.ok(repository.findAll(page));  //Serve para criar uma páginação do banco de dados
 	
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Country> getOne(@PathVariable("id") Long id) {
+		
+		return ResponseEntity.ok(repository.findById(id).get());
 	}
 	
 //	Não apsse adiante a
